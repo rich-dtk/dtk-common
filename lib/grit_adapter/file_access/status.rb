4d1e19185c41cb4c3b07c1531ce6c73bcf2369b0
Rich Pelavin <rich.pelavin@gmail.com>
2012-07-24 22:43:52 +0000

    put in methods to check if any status changes and to shift untyracked to added

--- lib/grit_adapter/file_access/status.rb
+++ lib/grit_adapter/file_access/status.rb
@@ -9,12 +9,24 @@ module DTK::Common; class GritAdapter; class FileAccess
     class Status < Hash
       def initialize(grit_status_obj)
         super()
-        [:added,:deleted,:changed,:untracked].each do |file_state|
-#          paths_with_file_state = grit_status_obj.send(file_state).map{|info|info[0]} 
+        FileStates.each do |file_state|
           paths_with_file_state = grit_status_obj.send(file_state).map{|info|info[1].path} 
           self[file_state] = paths_with_file_state unless paths_with_file_state.empty?
         end
       end
+      
+      def any_changes?()
+        !!FileStates.find{|fs|self[fs]}
+      end
+      
+      def shift_untracked_to_added!()
+        if untracked = self.delete(:untracked)
+          self[:added] = (self[:added]||[]) + untracked
+        end
+        self
+      end
+
+      FileStates = [:added,:deleted,:changed,:untracked]
     end
   end                                         
 end; end; end
