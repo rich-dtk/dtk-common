e5403c57094a140bf063ae313f203a774d5ad75d
Haris KRAJINA <haris@atlantbh.com>
2013-01-31 15:48:41 +0100

    Fix final

--- lib/dtk-common.rb
+++ lib/dtk-common.rb
@@ -1,7 +1,9 @@
 module DTK
   module Common
-    Dir.glob('../lib/**/*.rb') do |file|
-      require file unless file.include?('dtk-common.rb')
+    require File.expand_path('require_first.rb', File.dirname(__FILE__))
+
+    Dir.glob("#{File.dirname(__FILE__)}/**/*.rb") do |file|
+      require file unless file.include?('dtk-common.rb') || file.include?('file_access/') || file.include?('require_first.rb')
     end
   end
 end
