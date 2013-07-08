8387f7d033a478fc319e4c49e05fec78d3c7171c
Rich Pelavin <rich@reactor8.com>
2013-07-08 17:50:49 +0000

    feature_dsl_rugged: put in logic that dynamically checks if rugged gem is installed

--- lib/git_repo.rb
+++ lib/git_repo.rb
@@ -13,7 +13,7 @@ module DtkCommon
 
    private
     def method_missing(name,*args,&block)
-      if adapter_name = AdaptersForMethods[name]
+      if adapter_name = get_adapter_name(name)
         execution_wrapper do
           adapter_instance = @adapters[adapter_name] ||= get_adapter_class(adapter_name).new(@repo_path)
           adapter_instance.send(name,*args,&block)
@@ -30,7 +30,7 @@ module DtkCommon
         raise e
        rescue => e
         if e.respond_to?(:to_s)
-          raise DtkCommon::Error.new(e.to_s)
+          raise ::DtkCommon::Error.new(e.to_s)
         else
           raise e
         end
@@ -45,8 +45,21 @@ module DtkCommon
       DynmamicLoader.load_and_return_adapter_class(:git_repo,adapter_name,:base_class => Adapter)
     end
 
+    def get_adapter_name(method_name)
+      ret = Array(AdaptersForMethods[method_name]||[]).find do |adapter_name|
+        condition = AdapterConditions[adapter_name]
+        condition.nil? or condition.call()
+      end
+      ret || raise(Error.new("Cannot find applicable adapter for method (#{method_name})"))
+    end
+
+    #for each hash value form is scalar or array of adapters to try in order
     AdaptersForMethods = {
       :get_file_content => :rugged
+    }
+
+    AdapterConditions = {
+      :rugged => proc{!Gem::Specification::find_all_by_name('rugged').empty?}
     }
   end
 end
